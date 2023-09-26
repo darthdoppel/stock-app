af78fe46f9201180ac42678ca370800016b3925f
Leandro Magallanes <leandromartinm7@gmail.com>
2023-09-25 21:24:17 -0300

    updates on server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 const corsOptions = {
-  origin: ['https://stock-app-client.onrender.com'],
+  origin: ['https://stock-app-client.onrender.com', 'http://localhost:5173'],
   optionsSuccessStatus: 200
 }
 
